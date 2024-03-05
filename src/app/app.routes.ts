cdf12706e3c3a56d7d9bf507f580204eef5d17da
Vedanth Subramaniam <vedanth.1113@gmail.com>
2024-03-04 22:40:46 -0800

    Navbar component deffined

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,8 +3,4 @@ import {StockSearchComponent} from "./stock-search/stock-search.component";
 import {StockWishlistComponent} from "./stock-wishlist/stock-wishlist.component";
 import {StockPortfolioComponent} from "./stock-portfolio/stock-portfolio.component";
 
-export const routes: Routes = [
-  { path: 'search-component', component: StockSearchComponent },
-  { path: 'wishlist-component', component: StockWishlistComponent },
-  { path: 'portfolio-component', component:StockPortfolioComponent}
-];
+export const routes: Routes = [];
