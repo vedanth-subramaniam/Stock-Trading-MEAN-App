23dc7dd49d3e4a9ce2f7b68ba5d9ad24100992db
Vedanth Subramaniam <vedanth.1113@gmail.com>
2024-03-09 18:43:05 -0800

    Before changing search form

--- src/app/stock-search/stock-search.component.ts
+++ src/app/stock-search/stock-search.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-stock-search',
@@ -7,6 +7,11 @@ import { Component } from '@angular/core';
   templateUrl: './stock-search.component.html',
   styleUrl: './stock-search.component.css'
 })
-export class StockSearchComponent {
+export class StockSearchComponent implements OnInit {
+
+
+  ngOnInit(): void {
+    console.log("Hey");
+  }
 
 }
