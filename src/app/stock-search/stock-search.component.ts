e271b3117e7cb8fd909e6fb766bc5e2fe1c6a512
Vedanth Subramaniam <vedanth.1113@gmail.com>
2024-03-09 19:27:04 -0800

    search input changes

--- src/app/stock-search/stock-search.component.ts
+++ src/app/stock-search/stock-search.component.ts
@@ -1,17 +1,34 @@
 import {Component, OnInit} from '@angular/core';
+import {FormsModule} from "@angular/forms";
+import {HttpClientModule} from "@angular/common/http";
 
 @Component({
   selector: 'app-stock-search',
   standalone: true,
-  imports: [],
+  imports: [
+    FormsModule, HttpClientModule
+  ],
   templateUrl: './stock-search.component.html',
   styleUrl: './stock-search.component.css'
 })
 export class StockSearchComponent implements OnInit {
 
+  tickerSymbol: string = '';
+
+  constructor() {
+  }
 
   ngOnInit(): void {
     console.log("Hey");
   }
 
+  searchStock() {
+    console.log('Searching for stock:', this.tickerSymbol);
+    // console.log(this.stockApiService.getCompanyCommonDetails(this.tickerSymbol));
+  }
+
+  clearSearch() {
+    this.tickerSymbol = '';
+    // Implement the logic to clear out the currently searched results and show the initial search page
+  }
 }
