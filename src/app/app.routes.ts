6507b953bc9bfa91b6efa33ebb1667c207ef7d73
Vedanth Subramaniam <vedanth.1113@gmail.com>
2024-03-12 16:17:58 -0700

    changing to routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,11 @@
-import { Routes } from '@angular/router';
+import {Routes} from '@angular/router';
 import {StockSearchComponent} from "./stock-search/stock-search.component";
 import {StockWishlistComponent} from "./stock-wishlist/stock-wishlist.component";
 import {StockPortfolioComponent} from "./stock-portfolio/stock-portfolio.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: 'search', component: StockSearchComponent},
+  {path: 'wishlist', component: StockWishlistComponent},
+  {path: 'portfolio', component: StockPortfolioComponent},
+  {path: '', component: StockSearchComponent}
+];
