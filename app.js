12d5c3e0d2b77a07256f976ed092147afa95f516
Vedanth Subramaniam <vedanth.1113@gmail.com>
2024-03-24 23:00:11 -0700

    Hourly chart added and Date modifications to APIs

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
 const axios = require('axios');
 
 require('dotenv').config();
 const API_KEY = process.env.API_KEY;
 
+app.use(cors());
 app.get('/search/:stockTicker', async function (req, res) {
 
     const stockTickerSymbol = req.params.stockTicker;
@@ -50,6 +52,39 @@ app.get('/search/:stockTicker', async function (req, res) {
     }
 });
 
+app.get('/chartsHourly/:stockTicker', async function (req, res) {
+
+    const stockTickerSymbol = req.params.stockTicker;
+    console.log("Charts API");
+    try {
+        const multiplier = 1;
+        const timespan = 'hour';
+        
+
+        const to_date = new Date(new Date() - 24 * 60 * 60 * 1000); // 1 day ago
+        const from_date = new Date(to_date - 48 * 60 * 60 * 1000); // 24 hours ago
+        
+        console.log("To date is:", to_date);
+        console.log("From date is:", from_date);
+
+        const to_date_str = to_date.toISOString().split('T')[0];
+        const from_date_str = from_date.toISOString().split('T')[0];
+        
+        const api_url = `https://api.polygon.io/v2/aggs/ticker/${stockTickerSymbol}/range/${multiplier}/${timespan}/${from_date_str}/${to_date_str}?adjusted=true&sort=asc&apiKey=mwjPq5ceMqn18edHgYpUrK5ZgBul4p2v`;
+        console.log(api_url);
+        const response = await axios.get(api_url);
+        const chartsHourlyResponse = convertTimestampToPST(response.data);
+
+        if (!chartsHourlyResponse) {
+            return res.status(400).json({ error: 'Chart details not found. Please check stock ticker symbol again.' });
+        }
+
+        res.send(chartsHourlyResponse);
+
+    } catch (error) {
+        res.status(500).json({ error: 'Something went wrong' });
+    }
+});
 
 app.get('/charts/:stockTicker', async function (req, res) {
 
@@ -59,8 +94,11 @@ app.get('/charts/:stockTicker', async function (req, res) {
         const multiplier = 1;
         const timespan = 'day';
 
-        const to_date = new Date();
+        const to_date = new Date(new Date() - 24 * 60 * 60 * 1000);
         const from_date = new Date(to_date - 2 * 365 * 24 * 60 * 60 * 1000); // 2 years ago
+
+        console.log("To date is:", to_date);
+        console.log("From date is:", from_date);
 
         const to_date_str = to_date.toISOString().split('T')[0];
         const from_date_str = from_date.toISOString().split('T')[0];
@@ -87,7 +125,7 @@ app.get('/news/:stockTicker', async function (req, res) {
 
     try {
         const stockTicker = req.params.stockTicker;
-        const now = new Date();
+        const now = new Date(new Date() - 24 * 60 * 60 * 1000);
         const from_date = new Date(now - 7 * 24 * 60 * 60 * 1000); // 7 days ago
 
         const from_date_str = from_date.toISOString().split('T')[0];
@@ -109,7 +147,7 @@ app.get('/news/:stockTicker', async function (req, res) {
             return res.status(400).json({ error: 'Company news not found' });
         }
 
-        res.send(companyNews);
+        res.send(companyNews.slice(0, 20));
 
     } catch (error) {
         console.error('Error fetching company news:', error.message);
@@ -133,7 +171,7 @@ app.get('/insights/:stockTicker', async function (req, res) {
 
         console.log(recommendationResponse);
 
-        const now = new Date();
+        const now = new Date(new Date() - 24 * 60 * 60 * 1000);
         const to_date_str = now.toISOString().split('T')[0];
         const from_date_str = "2022-01-01";
 
@@ -189,7 +227,23 @@ app.get('/autoComplete/:query', async function (req, res) {
 });
 
 function filterCompleteEntries(articles) {
-    return articles.filter(article => Object.values(article).every(value => value !== ''));
+    // Filter articles that do not have an empty title, an empty image URL, and image URL not containing "s.yimg.com"
+    return articles.filter(article =>
+        article.title !== '' &&
+        article.image !== '' &&
+        !article.image.includes('s.yimg.com')
+    );
+}
+
+function convertTimestampToPST(response) {
+    const results = response.results.map(item => {
+        const date = new Date(item.t);
+        const pstDate = date.toLocaleString("en-US", { timeZone: "America/Los_Angeles" });
+        console.log("PST Date is:", pstDate);
+        return { ...item, t: pstDate };
+    });
+
+    return { ...response, results };
 }
 
 app.listen('3000', () => {
