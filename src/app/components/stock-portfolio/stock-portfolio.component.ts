d97878ecf8ebbb4591ec0ec996d3c196ca6d3ec3
Vedanth Subramaniam <vedanth.1113@gmail.com>
2024-03-26 23:27:20 -0700

    Stock portfolio init

--- src/app/components/stock-portfolio/stock-portfolio.component.ts
+++ src/app/components/stock-portfolio/stock-portfolio.component.ts
@@ -1,44 +1,53 @@
 import {Component, OnInit} from '@angular/core';
 import {StockApiService} from "../../services/stock-api.service";
+import {CurrencyPipe, NgForOf} from "@angular/common";
 
 @Component({
   selector: 'app-stock-portfolio',
   standalone: true,
-  imports: [],
+  imports: [
+    CurrencyPipe,
+    NgForOf
+  ],
   templateUrl: './stock-portfolio.component.html',
   styleUrl: './stock-portfolio.component.css'
 })
 export class StockPortfolioComponent implements OnInit {
 
   walletBalance: any;
+  portFolioDataList: any;
+
+
   constructor(private stockService: StockApiService) {
   }
 
   ngOnInit(): void {
     this.stockService.getPortfolioData().subscribe({
       next:(response) =>{
-        console.log(response);
-        // Iterate over the data
+        this.portFolioDataList = response;
+        console.log(this.portFolioDataList);
       }
     });
 
     this.stockService.getWalletBalanceDB().subscribe({
-      next: (response) =>{
-        console.log(response);
+      next: (response: any) =>{
+        this.walletBalance = response.balance;
         // Update the wallet balance
       }
     });
   }
 
-  buyStock(){
+  buyStock(stock: any){
     // Do the calculation to check if enough funds are there to purchase the stock
     // If yes then update the details and quanity and send it to the DB.
     // If no then error
+    console.log(stock);
   }
 
-  sellStock(){
+  sellStock(stock: any){
     // Do the calculation and update the wallet balance
     // Update the records in the DB
+    console.log(stock);
   }
 }
 
