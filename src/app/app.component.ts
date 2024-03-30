a46847bcc4ef2815feacf2e166f855bb5377b62a
Vedanth Subramaniam <vedanth.1113@gmail.com>
2024-03-30 16:34:05 -0700

    Routing from wishlist formatting

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import {RouterLink, RouterOutlet} from '@angular/router';
+import {RouterLink, RouterLinkActive, RouterOutlet} from '@angular/router';
 import {StockWishlistComponent} from "./components/stock-wishlist/stock-wishlist.component";
 import {StockPortfolioComponent} from "./components/stock-portfolio/stock-portfolio.component";
 import {StockSearchComponent} from "./components/stock-search/stock-search.component";
@@ -9,7 +9,7 @@ import {NgbModule} from "@ng-bootstrap/ng-bootstrap";
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, StockWishlistComponent, StockPortfolioComponent, StockSearchComponent, NgIf, NgbModule, RouterLink, NgSwitch],
+  imports: [RouterOutlet, StockWishlistComponent, StockPortfolioComponent, StockSearchComponent, NgIf, NgbModule, RouterLink, NgSwitch, RouterLinkActive],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
